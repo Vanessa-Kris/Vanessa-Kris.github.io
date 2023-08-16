119919aff80b54a6a18a1d669adcfe1c72e4a658
Minion <nessakristo@gmail.com>
2023-08-16 17:13:22 +0100

    added contact links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,74 +17,116 @@ const Contact = () => {
     <Box sx={{ height: { md: "100vh", xs: "100%" }, py: 15, px: 4 }}>
       <Grid container spacing={4}>
         <Grid item md={4} xs={12}>
-          <Box sx={{ bgcolor: "#38369A", p: 5 }}>
-            <FontAwesomeIcon
-              bounce
-              style={{ paddingRight: 12 }}
-              icon={faEnvelope}
-            />
-            Email
-          </Box>
+          <a
+            href="mailto:vanessaigwe1@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box sx={{ bgcolor: "#38369A", p: 5 }}>
+              <FontAwesomeIcon
+                bounce
+                style={{ paddingRight: 12 }}
+                icon={faEnvelope}
+              />
+              Email
+            </Box>
+          </a>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box sx={{ bgcolor: "#38369A", p: 5 }}>
-            <FontAwesomeIcon
-              bounce
-              style={{ paddingRight: 12 }}
-              icon={faWhatsapp}
-            />
-            WhatsApp
-          </Box>
+          <a
+            href="https://wa.link/1r1q4f"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box sx={{ bgcolor: "#38369A", p: 5 }}>
+              <FontAwesomeIcon
+                bounce
+                style={{ paddingRight: 12 }}
+                icon={faWhatsapp}
+              />
+              WhatsApp
+            </Box>
+          </a>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box sx={{ bgcolor: "#38369A", p: 5 }}>
-            <FontAwesomeIcon
-              bounce
-              style={{ paddingRight: 12 }}
-              icon={faGithub}
-            />
-            GitHub
-          </Box>
+          <a
+            href="https://github.com/Vanessa-Kris"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box sx={{ bgcolor: "#38369A", p: 5 }}>
+              <FontAwesomeIcon
+                bounce
+                style={{ paddingRight: 12 }}
+                icon={faGithub}
+              />
+              GitHub
+            </Box>
+          </a>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box sx={{ bgcolor: "#38369A", p: 5 }}>
-            <FontAwesomeIcon
-              bounce
-              style={{ paddingRight: 12 }}
-              icon={faFacebook}
-            />
-            Facebook
-          </Box>
+          <a
+            href="https://web.facebook.com/vanessa.igwe.35"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box sx={{ bgcolor: "#38369A", p: 5 }}>
+              <FontAwesomeIcon
+                bounce
+                style={{ paddingRight: 12 }}
+                icon={faFacebook}
+              />
+              Facebook
+            </Box>
+          </a>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box sx={{ bgcolor: "#38369A", p: 5 }}>
-            <FontAwesomeIcon
-              bounce
-              style={{ paddingRight: 12 }}
-              icon={faTwitter}
-            />
-            Twitter
-          </Box>
+          <a
+            href="https://twitter.com/Mini_on_Nessa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box sx={{ bgcolor: "#38369A", p: 5 }}>
+              <FontAwesomeIcon
+                bounce
+                style={{ paddingRight: 12 }}
+                icon={faTwitter}
+              />
+              Twitter
+            </Box>
+          </a>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box sx={{ bgcolor: "#38369A", p: 5 }}>
-            <FontAwesomeIcon
-              bounce
-              style={{ paddingRight: 12 }}
-              icon={faLinkedin}
-            />
-            LinkedIn
-          </Box>
+          <a
+            href="https://www.linkedin.com/in/vanessa-christopher-igwe-712807218/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box sx={{ bgcolor: "#38369A", p: 5 }}>
+              <FontAwesomeIcon
+                bounce
+                style={{ paddingRight: 12 }}
+                icon={faLinkedin}
+              />
+              LinkedIn
+            </Box>
+          </a>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box sx={{ bgcolor: "#38369A", p: 5 }}>
-            <FontAwesomeIcon
-              bounce
-              style={{ paddingRight: 12 }}
-              icon={faInstagram}
-            />
-            Instagram
-          </Box>
+          <a
+            href="https://www.instagram.com/vanessa_christopher_i"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box sx={{ bgcolor: "#38369A", p: 5 }}>
+              <FontAwesomeIcon
+                bounce
+                style={{ paddingRight: 12 }}
+                icon={faInstagram}
+              />
+              Instagram
+            </Box>
+          </a>
         </Grid>
       </Grid>
     </Box>
