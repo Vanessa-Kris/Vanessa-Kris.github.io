9807e0b425d12e7ae14567ae559d46bbece48730
Minion <nessakristo@gmail.com>
2023-10-12 10:45:43 +0100

    added error page

--- src/Pages/404.js
+++ src/Pages/404.js
@@ -1,5 +1,15 @@
+import { Box, Typography, Button } from "@mui/material";
 import React from "react";
 
 export default function PageNotFound() {
-  return <div>404</div>;
+  return (
+    <Box align="center" sx={{ mt: { md: 40, sm: 40, xs: 30 } }}>
+      <Typography variant="h2" sx={{ pb: 3 }}>
+        Damn! You got lost
+      </Typography>
+      <Button component="a" href="/" variant="contained">
+        Let's get you back home
+      </Button>
+    </Box>
+  );
 }
